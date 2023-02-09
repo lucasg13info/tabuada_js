f0eda605d4f0b14661909d0b987513c03970226e
Lucas Estefano <lucccasestefano_@hotmail.com>
2023-02-09 20:58:02 -0300

    Tabuada

---
+++ loops.js
@@ -0,0 +1,66 @@
+function tabuada(){
+    // X1
+    document.write("<h2>Tabuada do 1 </h2>");
+    for(var count=1; count<=10 ; count++){
+        document.write("1 x "+count+" = " + (1*count) + "<br />");
+    
+    }
+    
+    // X2
+    document.write("<h2>Tabuada do 2 </h2>");
+    for(var count=1; count<=10 ; count++){
+        document.write("2 x "+count+" = " + (2*count) + "<br />");
+    }
+
+    // X3
+    document.write("<h2>Tabuada do 3 </h2>");
+    for(var count=1; count<=10 ; count++){
+        document.write("3 x "+count+" = " + (3*count) + "<br />");
+    }
+
+    // X4
+    document.write("<h2>Tabuada do 4 </h2>");
+    for(var count=1; count<=10 ; count++){
+        document.write("4 x "+count+" = " + (4*count) + "<br />");
+    }
+    
+    // X5
+    document.write("<h2>Tabuada do 5 </h2>");
+    for(var count=1; count<=10 ; count++){
+        document.write("5 x "+count+" = " + (5*count) + "<br />");
+    }
+
+
+    // X6
+    document.write("<h2>Tabuada do 6 </h2>");
+    for(var count=1; count<=10 ; count++){
+        document.write("6 x "+count+" = " + (6*count) + "<br />");
+    }
+
+    // X7
+    document.write("<h2>Tabuada do 7 </h2>");
+    for(var count=1; count<=10 ; count++){
+        document.write("7 x "+count+" = " + (7*count) + "<br />");
+    }
+
+    // X8
+    document.write("<h2>Tabuada do 8 </h2>");
+    for(var count=1; count<=10 ; count++){
+        document.write("8 x "+count+" = " + (8*count) + "<br />");
+    }
+
+    // X9
+    document.write("<h2>Tabuada do 9 </h2>");
+    for(var count=1; count<=10 ; count++){
+        document.write("9 x "+count+" = " + (9*count) + "<br />");
+    }
+
+    // X10
+    document.write("<h2>Tabuada do 10 </h2>");
+    for(var count=1; count<=10 ; count++){
+        document.write("10 x "+count+" = " + (10*count) + "<br />");
+    }
+  }
+
+
+tabuada()
